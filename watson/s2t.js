1de2b958c3f2299f05abbeec6a93f6f3b7d2e71d
arlemi <aturpaul@fr.ibm.com>
2016-04-12 00:31:42 +0200

    edit variable names to comply with guidelines

--- watson/s2t.js
+++ watson/s2t.js
@@ -42,8 +42,8 @@ module.exports = function (RED) {
 
     this.on('input', function (msg) {
       if (!msg.payload) {
-        var message = 'Missing property: msg.payload';
-        node.error(message, msg)
+        var message_err_payload = 'Missing property: msg.payload';
+        node.error(message_err_payload, msg)
         return;
       }
 
@@ -51,8 +51,8 @@ module.exports = function (RED) {
       password = password || this.credentials.password;
 
       if (!username || !password) {
-        var message = 'Missing Speech To Text service credentials';
-        node.error(message, msg)
+        var message_err_credentials = 'Missing Speech To Text service credentials';
+        node.error(message_err_credentials, msg)
         return;
       }
 
@@ -63,20 +63,20 @@ module.exports = function (RED) {
       }
 
       if (!config.lang) {
-        var message = 'Missing audio language configuration, unable to process speech.';
-        node.error(message, msg)
+        var message_err_lang = 'Missing audio language configuration, unable to process speech.';
+        node.error(message_err_lang, msg)
         return;
       }
 
       if (!config.band) {
-        var message = 'Missing audio band configuration, unable to process speech.';
-        node.error(message, msg)
+        var message_err_band = 'Missing audio band configuration, unable to process speech.';
+        node.error(message_err_band, msg)
         return;
       }
 
       if (!config.continuous) {
-        var message = 'Missing continuous details, unable to process speech.';
-        node.error(message, msg)
+        var message_err_continuous = 'Missing continuous details, unable to process speech.';
+        node.error(message_err_continuous, msg)
         return;
       }
 
@@ -91,13 +91,13 @@ module.exports = function (RED) {
 
       var s2t = function (audio, format, cb) {
         if (format !== 'wav' && format !== 'flac' && format !== 'ogg') {
-          var message = 'Audio format (' + format + ') not supported, must be encoded as WAV, FLAC or OGG.';
-          node.error(message, msg)
+          var message_err_format = 'Audio format (' + format + ') not supported, must be encoded as WAV, FLAC or OGG.';
+          node.error(message_err_format, msg)
           return;
         }
 
         if (format === 'ogg') format += ';codecs=opus';
-console.log(config.continuous);
+
         var params = {
           audio: audio,
           content_type: 'audio/' + format,
