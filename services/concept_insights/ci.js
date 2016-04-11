48d92d70fc7575d5f12a9938337335c8c294d392
Daniel Cunnington <dancunnington@uk.ibm.com>
2016-04-11 21:15:18 +0100

    Concept insights node v1

--- services/concept_insights/ci.js
+++ services/concept_insights/ci.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 IBM Corp.
+ * Copyright 2016 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,13 +14,12 @@
  * limitations under the License.
  **/
 
- module.exports = function (RED) {
+module.exports = function (RED) {
   var request = require('request');
   var cfenv = require('cfenv');
   var watson = require('watson-developer-cloud');
 
   var username, password;
-
   var service = cfenv.getAppEnv().getServiceCreds(/concept insights/i);
 
   if (service) {
@@ -32,26 +31,24 @@
     res.json(service ? {bound_service: true} : null);
   });
 
-  function ConceptInsightsNode(config) {
+  function CorpusConfigurationNode(config) {
     RED.nodes.createNode(this,config);
     this.cname = config.cname;
     this.access = config.access;
     this.publiccorpus = config.publiccorpus;
   }
 
-  RED.nodes.registerType("watson-concept-insights-corpus",ConceptInsightsNode);
+  RED.nodes.registerType("watson-concept-insights-corpus", CorpusConfigurationNode);
 
   function searchNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
 
     this.on('input', function(msg) {
-
       setupConceptInsightsNode(msg,config,this,function(concept_insights,account_id) {
-
         this.corpus = RED.nodes.getNode(config.corpus);
+        
         if (this.corpus) {
-
           var corpus_name;
           if (this.corpus.cname) {
             corpus_name = this.corpus.cname;
@@ -63,7 +60,15 @@
           var document_name = config.docname;
 
           node.status({fill:"blue",shape:"ring",text:"requesting..."});
+
+          //Check for document or corpus mode
           if (config.mode === 'document') {
+
+            if (!document_name) {
+              node.status({});
+              var message = 'No document name specified';
+              return node.error(message, msg);
+            }
 
             var params = {
               id: '/corpora/'+account_id+'/'+corpus_name+'/documents/'+document_name
@@ -71,14 +76,12 @@
 
             switch (config.docapicall) {
               case 'annotations':
-
                 params.id = params.id+'/annotations';
-                
-                //Request annotations from concept insights api
                 concept_insights.corpora.getDocumentAnnotations(params, function(err,res) {
                   handleWatsonCallback(node,msg,err,res);
                 });
                 break;
+
               case 'relatedconcepts':
                 concept_insights.corpora.getRelatedConcepts(params, function(err,res) {
                   handleWatsonCallback(node,msg,err,res);
@@ -91,11 +94,9 @@
                   handleWatsonCallback(node,msg,err,res);
                 });
                 break;
-
             }
-
           } else {
-            //corpus
+            //Corpus mode
             var params = {
               corpus: '/corpora/'+account_id+'/'+corpus_name
             }
@@ -105,11 +106,13 @@
                   handleWatsonCallback(node,msg,err,res);
                 });
                 break;
+
               case 'relatedconcepts':
                 concept_insights.corpora.getRelatedConcepts(params, function(err,res) {
                   handleWatsonCallback(node,msg,err,res);
                 });
                 break;
+
               case 'relationscores':
                 params.concepts = msg.concepts;
                 concept_insights.corpora.getRelationScores(params, function(err,res) {
@@ -123,36 +126,37 @@
                   concept_insights.corpora.getRelatedDocuments(params, function(err,res) {
                     handleWatsonCallback(node,msg,err,res);
                   });
-                  break;
-
                 } else {
                   node.status({});
-                  var message = 'No ids on the msg object';
-                  node.error(message, msg)
-                  break;
+                  var message = 'No concepts exist on the msg object';
+                  node.error(message, msg);
                 }
-
-              case 'documentlabel':
-                params.prefix=true;
-                params.query=config.query;
-                concept_insights.corpora.searchByLabel(params, function(err,res) {
-                    handleWatsonCallback(node,msg,err,res);
-                  });
                 break;
 
-                default:
-                  return;
+              case 'documentlabel':
+
+                if (params.query) {
+                  params.prefix=true;
+                  params.query=config.query;
+                  concept_insights.corpora.searchByLabel(params, function(err,res) {
+                      handleWatsonCallback(node,msg,err,res);
+                  });
+                } else {
+                  node.status({});
+                  var message = 'No query term specified';
+                  node.error(message, msg);
+                }
+                break;
+
+              default:
+                return;
             }
           }
-
         } else {
           var message = 'No corpus configuration specified';
-          node.error(message, msg)
-          return;
+          return node.error(message, msg);
         }
-        
       });
-
     });
   }
 
@@ -163,18 +167,6 @@
     }
   });
 
-  function handleWatsonCallback(node,msg,err,res) {
-    node.status({});
-    if (err) {
-      var message = err.error;
-      node.error(message, msg)
-      return;
-    } else {
-      msg.payload = res;
-      node.send(msg);
-    }
-  }
-
   function searchConceptsNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
@@ -182,7 +174,11 @@
     this.on('input', function(msg) {
       setupConceptInsightsNode(msg,config,this,function(concept_insights,account_id) {
         
-        //set up parameters
+        //Set up parameters, searching the latest wikipedia graph
+        if (!config.query) {
+          var message = "No query term specified";
+          return node.error(message, msg);
+        }
         var params = {
           graph: '/graphs/wikipedia/en-latest',
           query: config.query,
@@ -194,11 +190,10 @@
           node.status({});
           if (err) {
             var message = err.error;
-            node.error(message, msg)
-            return;
+            return node.error(message, msg);
           } else {
             
-            //Build up array of concept ids
+            //Build up array of concept ids to attach to msg.concepts
             var concepts = [];
             var matches = res.matches;
             for (var i=0; i<matches.length; i++) {
@@ -211,7 +206,6 @@
         });
       });
     });
-    
   }
 
   RED.nodes.registerType('watson-concept-insights-search-concepts', searchConceptsNode, {
@@ -237,17 +231,14 @@
           node.status({});
           if (err) {
             var message = err.error;
-            node.error(message, msg)
-            return;
+            return node.error(message, msg);
           } else {
             msg.payload = res;
             node.send(msg);
           }
-        });
-        
+        }); 
       });
-    });
-    
+    }); 
   }
 
   RED.nodes.registerType('watson-concept-insights-related-concepts', relatedConceptsNode, {
@@ -258,15 +249,13 @@
   });
 
 
-  function Node (config) {
+  function uploadDocumentNode (config) {
     RED.nodes.createNode(this, config);
     var node = this;
 
     this.on('input', function (msg) {
 
       setupConceptInsightsNode(msg,config,this,function(concept_insights,account_id) {
-
-        // Retrieve the corpus config node
         this.corpus = RED.nodes.getNode(config.corpus);
         if (this.corpus) {
           var corpus_name = this.corpus.cname;
@@ -280,89 +269,91 @@
             corpus: '/corpora/'+account_id+'/'+corpus_name,              
           }
 
-          //Check if corpus exists
+          //If corpus doesn't exist, create it
           concept_insights.corpora.getCorpus(params, function(err,res) {
             if (err && err.error === 'not found') {
-
-                //Create corpus
-                params.access = access;
-                node.status({fill:"blue",shape:"ring",text:"Creating corpus "+corpus_name});
-                concept_insights.corpora.createCorpus(params, function(err,resp) {
-
-                  if (err) {
-                    var message = err.error;
-                    node.error(message, msg)
-                    return;
-                  }
-                  addDocument(checkStatus);
-                });
-
-              } else {
-                node.status({fill:"blue",shape:"ring",text:"Using existing corpus "+corpus_name});
+              //Create corpus
+              params.access = access;
+              node.status({fill:"blue",shape:"ring",text:"Creating corpus "+corpus_name});
+              concept_insights.corpora.createCorpus(params, function(err,resp) {
+                if (err) {
+                  var message = "Creating Corpus error: "+err.error;
+                  return node.error(message, msg);
+                } else {
+                  addDocument(checkStatus(msg.payload.length));
+                }
+              });
+            } else {
+              node.status({fill:"blue",shape:"ring",text:"Using existing corpus "+corpus_name});
+              if (account_id != "public") {
                 addDocument(checkStatus(msg.payload.length));
-              }
-            });
+              } else {
+                node.status({});
+                var message = "Permission denied";
+                return node.error(message, msg);
+              } 
+            }
+          });
         } else {
           var message = 'No corpus information specified';
-          node.error(message, msg)
-          return;
+          return node.error(message, msg);
         }
 
-
         function addDocument(cb) {
-          //Create document 
           var document_name= config.documentname;
           var document_label= config.documentlabel;
           var content_type = config.contenttype;
-          params = {
-            'id': '/corpora/'+account_id+'/'+corpus_name+'/documents/'+document_name,
-            'document': {
-              'label': document_label,
-              'parts': [
-              {
-                'name': 'part-one',
-                'content-type': content_type,
-                'data': msg.payload
+          //Check fields are populated
+          if (document_name && document_label && content_type) {
+            params = {
+              'id': '/corpora/'+account_id+'/'+corpus_name+'/documents/'+document_name,
+              'document': {
+                'label': document_label,
+                'parts': [
+                {
+                  'name': 'part-one',
+                  'content-type': content_type,
+                  'data': msg.payload
+                }
+                ]
               }
-              ]
-            }
+            };
+            node.status({fill:"blue",shape:"ring",text:"Uploading document"});
+            concept_insights.corpora.createDocument(params, function(err,res) {
+              node.status({fill:"blue",shape:"ring",text:"Document uploaded. Started indexing"});
+              if (err) {
+                node.status({});
+                var message = "Creating document error: "+err.error;
+                return node.error(message, msg);
+              } else {
+                if (cb) cb(); 
+              }         
+            });
+          } else {
+            var message = "Creating document error: Missing document name, label or content type";
+            return node.error(message, msg);
           }
-
-          node.status({fill:"blue",shape:"ring",text:"Uploading document"});
-          concept_insights.corpora.createDocument(params, function(err,res) {
-            node.status({fill:"blue",shape:"ring",text:"Document uploaded. Started indexing"});
-            if (err) {
-              node.status({});
-              var message = err.error;
-              node.error(message, msg)
-              return;
-            }
-            if (cb) cb();            
-          });
-        }
+        }          
 
         function checkStatus(fileLength) {
-
           //Calculate interval based on length of file, default to 10 seconds
           var interval = 10000;
           if (fileLength > 0) interval = (2*fileLength) + 5;
           
-
-          //Loop every X secs and check the status of the document 
+          //Loop and check the status of the document 
           var statusInterval = setInterval(function(){
             concept_insights.corpora.getDocumentProcessingState(params, function(err,res) {
               if (err) {
                 node.status({});
-                var message = err.error;
-                node.error(message, msg)
-                return;
+                var message = "Document processing state error: "+err.error;
+                clearInterval(statusInterval);
+                return node.error(message, msg);
               } else {
                 //get status
                 if (res.status == 'ready') {
                   node.status({fill:"green",shape:"ring",text:"Document ready"});
                   return clearInterval(statusInterval);
                 }
-
               }
             });
           },interval);
@@ -371,7 +362,7 @@
     });
   }
 
-  RED.nodes.registerType('watson-concept-insights', Node, {
+  RED.nodes.registerType('watson-concept-insights-upload-document', uploadDocumentNode, {
     credentials: {
       username: {type:"text"},
       password: {type:"password"}
@@ -393,8 +384,7 @@
 
     if (!username || !password) {
       var message = 'Missing Concept Insights service credentials';
-      node.error(message, msg)
-      return;
+      return node.error(message, msg);
     }
 
     //Connect to Watson
@@ -408,14 +398,24 @@
       node.status({fill:"blue",shape:"ring",text:"Getting account information"});
       if (err) {
         var message = err.error;
-        node.error(message, msg)
-        return;
+        return node.error(message, msg);
       } else {
         node.status({});
         var account_id = res.accounts[0].account_id;
         if (callback) callback(concept_insights,account_id);
       }
     });
+  }
+
+  function handleWatsonCallback(node,msg,err,res) {
+    node.status({});
+    if (err) {
+      var message = err.error;
+      return node.error(message, msg);
+    } else {
+      msg.payload = res;
+      node.send(msg);
+    }
   }
 };
 
