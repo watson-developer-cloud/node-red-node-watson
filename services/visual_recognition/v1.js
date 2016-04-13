700c1c4fb0b0a5afab7546e3082ddb18c08cda7c
Genaro Nieto Fernández <genaro@es.ibm.com>
2016-04-13 16:59:33 +0200

    Add latest release of convert, visual recognition and news

---
+++ services/visual_recognition/v1.js
@@ -0,0 +1,237 @@
+module.exports = function(RED) {
+
+	var cfEnv = require("cfenv");
+
+	var watson = require('watson-developer-cloud');
+	var temp = require('temp');
+	temp.track();
+	
+	var fs = require('fs');
+	var fileType = require('file-type');
+	var request = require('request');
+
+	var appEnv	= cfEnv.getAppEnv();
+		
+	var visual = [];
+	for (var i in appEnv.services) {
+		if (i.match(/^(visual_recognition)/i)) {
+			visual = visual.concat(appEnv.services[i].map(function(v) {
+				return {
+					name: v.name, 
+					label: v.label, 
+					username: v.credentials.username,
+					password: v.credentials.password,
+					};
+			}));
+		}
+	}
+
+	RED.httpAdmin.get('/watson-visual-recognition/vcap', function(req,res) {
+		res.send(JSON.stringify(visual));
+	});
+	
+	RED.httpAdmin.get('/watson-visual-recognition/list/:service', function(req,res) {
+
+		var username,password;
+		var service = req.params.service;
+		
+		for (var i2=0; i2 < visual.length; i2++) {
+			if (visual[i2].name===service) {
+				username = visual[i2].username;
+				password = visual[i2].password;
+			}
+		}
+		
+		var visual_recognition = watson.visual_recognition({
+			username: username,
+			password: password,
+			version: 'v2-beta',
+			version_date: '2015-12-02'
+		});
+			
+		visual_recognition.listClassifiers({},
+			function(err, response) {
+				if (err) 
+					res.send(err);
+			 	else
+					res.send(JSON.stringify(response));
+			}
+		);
+	});
+
+	function VisualUtilNode(config) {
+		
+		RED.nodes.createNode(this,config);
+
+		this.name = config.name;
+		this.classifier = config.classifier;
+		this.command = config.command;
+		this.username = config.username;
+		this.password = config.password;
+		this.service = config.service;		
+
+		var node = this;
+
+		this.doDelete = function(msg) {
+			var visual_recognition = watson.visual_recognition({
+			  username: node.username,
+			  password: node.password,
+			  version: 'v2-beta',
+			  version_date: '2015-12-02'
+			});
+			
+			visual_recognition.deleteClassifier({
+				classifier_id: node.classifier },
+				function(err, response) {
+					if (err) 
+						node.error(err);
+				 	else
+						node.send({"payload": response});
+				}
+			);
+		};
+
+		this.doList = function(msg) {
+
+			var visual_recognition = watson.visual_recognition({
+ 
+				username: node.username,
+				password: node.password,
+				version: 'v2-beta',
+				version_date: '2015-12-02'
+			});
+			
+			visual_recognition.listClassifiers({},
+				function(err, response) {
+					if (err) { 
+						node.error(err);
+				 	} else {
+						node.send({"payload": response});
+					}
+				}
+			);
+
+		};
+
+		this.doDetails = function(msg) {
+			var visual_recognition = watson.visual_recognition({
+			  username: node.username,
+			  password: node.password,
+			  version: 'v2-beta',
+			  version_date: '2015-12-02'
+			});
+
+			visual_recognition.getClassifier({
+				classifier_id: node.classifier },
+				function(err, response) {
+					if (err) 
+						node.error(err);
+				 	else
+						node.send({"payload": response});
+				}
+			);
+		};
+
+		this.on('input', function (msg) {
+			console.log("Visual command: "+node.command);
+			switch(this.command) {
+				case "list":
+					this.doList(msg);
+					break;
+				case "details":
+					this.doDetails(msg);
+					break;
+				case "delete":
+					this.doDelete(msg);
+					break;
+			}
+		});
+		
+	}
+
+	RED.nodes.registerType("watson-visual-util",VisualUtilNode);
+
+	function VisualTrainingNode(config) {
+
+		RED.nodes.createNode(this,config);
+
+		this.name = config.name;
+		this.classifier = config.classifier;
+		this.username = config.username;
+		this.password = config.password;
+		this.service = config.service;		
+
+		var node = this;
+
+		this.doCall = function(msg) {
+
+		var visual_recognition = watson.visual_recognition({
+			username: node.username,
+			password: node.password,
+			version: 'v2-beta',
+			version_date: '2015-12-02'
+		});
+
+		var stream_buffer = function (file, contents, cb) {
+			fs.writeFile(file, contents, function (err) {
+				if (err) throw err;
+				cb(fileType(contents).ext);
+			});
+		};
+
+		var stream_url = function (file, location, cb) {
+			var wstream = fs.createWriteStream(file);
+			wstream.on('finish', function () {
+				fs.readFile(file, function (err, buf) {
+					if (err) console.error(err);
+					cb(fileType(buf).ext);
+				});
+			});
+			request(location).pipe(wstream);
+		};
+			
+		var stream_positive = (typeof msg.positive === 'string') ? stream_url : stream_buffer;
+		var stream_negative = (typeof msg.negative === 'string') ? stream_url : stream_buffer;
+
+		temp.open({suffix: '.zip'}, function (err, info) {
+			if (err) throw err;
+			
+			stream_positive(info.path, msg.positive, function (format) {
+
+				temp.open({suffix: '.zip'}, function (err2, info2) {
+					if (err2) throw err2;
+
+					stream_negative(info2.path, msg.negative, function (format) {
+							
+						var params = {
+							name: node.classifier,
+							positive_examples: fs.createReadStream(info.path),
+							negative_examples: fs.createReadStream(info2.path)
+						};
+							
+						visual_recognition.createClassifier(params, 
+							function(err, response) {
+								if (err) {
+									node.error(err);
+								} else {
+									node.send({"payload" : response});
+								}
+								temp.cleanup();
+							}
+						);											
+						});
+					});
+				});
+			});
+		};
+
+		this.on('input', function (msg) {
+			this.doCall(msg);
+		});
+				
+	}
+
+	RED.nodes.registerType("watson-visual-training",VisualTrainingNode);
+	
+};
+
