1ca00b1f6baeee59ba61186a2213343aa705fdd6
Yacine Rezgui <rezgui.y@gmail.com>
2017-02-21 10:24:05 +0000

    Clean Personality Insights nodes

--- services/personality_insights/v3.js
+++ services/personality_insights/v3.js
@@ -14,9 +14,11 @@
  * limitations under the License.
  **/
 
-module.exports = function (RED) {
+module.exports = function(RED) {
   const SERVICE_IDENTIFIER = 'personality-insights';
-  var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3'),
+  var PersonalityInsightsV3 = require(
+    'watson-developer-cloud/personality-insights/v3',
+  ),
     payloadutils = require('../../utilities/payload-utils'),
     serviceutils = require('../../utilities/service-utils'),
     service = serviceutils.getServiceCreds(SERVICE_IDENTIFIER),
@@ -24,9 +26,20 @@ module.exports = function (RED) {
     password = null,
     sUsername = null,
     sPassword = null,
-
-    VALID_INPUT_LANGUAGES = ['ar','en','es','ja'],
-    VALID_RESPONSE_LANGUAGES = ['ar','de','en','es','fr','it','ja','ko','pt-br','zh-cn','zh-tw'];
+    VALID_INPUT_LANGUAGES = ['ar', 'en', 'es', 'ja'],
+    VALID_RESPONSE_LANGUAGES = [
+      'ar',
+      'de',
+      'en',
+      'es',
+      'fr',
+      'it',
+      'ja',
+      'ko',
+      'pt-br',
+      'zh-cn',
+      'zh-tw',
+    ];
 
   if (service) {
     sUsername = service.username;
@@ -35,8 +48,8 @@ module.exports = function (RED) {
 
   // This HTTP GET REST request is used by the browser side of the node to
   // determine if credentials are found.
-  RED.httpAdmin.get('/watson-personality-insights-v3/vcap', function (req, res) {
-    res.json(service ? {bound_service: true} : null);
+  RED.httpAdmin.get('/watson-personality-insights-v3/vcap', function(req, res) {
+    res.json(service ? { bound_service: true } : null);
   });
 
   // This function prepares the params object for the
@@ -47,12 +60,16 @@ module.exports = function (RED) {
       outputlang = config.outputlang ? config.outputlang : 'en';
 
     if (msg.piparams) {
-      if (msg.piparams.inputlanguage &&
-            -1 < VALID_INPUT_LANGUAGES.indexOf(msg.piparams.inputlanguage)) {
+      if (
+        msg.piparams.inputlanguage &&
+        -1 < VALID_INPUT_LANGUAGES.indexOf(msg.piparams.inputlanguage)
+      ) {
         inputlang = msg.piparams.inputlanguage;
       }
-      if (msg.piparams.responselanguage &&
-            -1 < VALID_RESPONSE_LANGUAGES.indexOf(msg.piparams.responselanguage)) {
+      if (
+        msg.piparams.responselanguage &&
+        -1 < VALID_RESPONSE_LANGUAGES.indexOf(msg.piparams.responselanguage)
+      ) {
         outputlang = msg.piparams.responselanguage;
       }
     }
@@ -64,8 +81,8 @@ module.exports = function (RED) {
       headers: {
         'content-language': inputlang,
         'accept-language': outputlang,
-        'accept': 'application/json'
-      }
+        accept: 'application/json',
+      },
     };
 
     return params;
@@ -74,33 +91,43 @@ module.exports = function (RED) {
   // This is the start of the Node Code. In this case only on input
   // is being processed.
   function Node(config) {
-    RED.nodes.createNode(this,config);
+    RED.nodes.createNode(this, config);
     var node = this,
       wc = payloadutils.word_count(config.inputlang),
       message = '';
 
-    this.on('input', function (msg) {
+    this.on('input', function(msg) {
       //var self = this;
 
       if (!msg.payload) {
         message = 'Missing property: msg.payload';
-        node.status({fill:'red', shape:'ring', text:'missing payload'});
+        node.status({ fill: 'red', shape: 'ring', text: 'missing payload' });
         node.error(message, msg);
         return;
       }
 
-      if ('string' !== typeof(msg.payload)) {
+      if ('string' !== typeof msg.payload) {
         message = 'submitted msg.payload is not text';
-        node.status({fill:'red', shape:'ring', text:'payload is not text'});
+        node.status({
+          fill: 'red',
+          shape: 'ring',
+          text: 'payload is not text',
+        });
         node.error(message, msg);
         return;
       }
 
-      wc(msg.payload, function (length) {
+      wc(msg.payload, function(length) {
         if (length < 100) {
           message = 'Personality insights requires a minimum of one hundred words.' +
-                            ' Only ' + length + ' submitted';
-          node.status({fill:'red', shape:'ring', text:'insufficient words submitted'});
+            ' Only ' +
+            length +
+            ' submitted';
+          node.status({
+            fill: 'red',
+            shape: 'ring',
+            text: 'insufficient words submitted',
+          });
           node.error(message, msg);
           return;
         }
@@ -110,7 +137,11 @@ module.exports = function (RED) {
 
         if (!username || !password) {
           message = 'Missing Personality Insights service credentials';
-          node.status({fill:'red', shape:'ring', text:'missing credentials'});
+          node.status({
+            fill: 'red',
+            shape: 'ring',
+            text: 'missing credentials',
+          });
           node.error(message, msg);
           return;
         }
@@ -119,30 +150,33 @@ module.exports = function (RED) {
           personality_insights = new PersonalityInsightsV3({
             username: username,
             password: password,
-            version_date: '2016-12-15'
+            version_date: '2016-12-15',
           });
 
-        node.status({fill:'blue', shape:'dot', text:'requesting'});
-        personality_insights.profile(params, function(err, response){
+        node.status({ fill: 'blue', shape: 'dot', text: 'requesting' });
+        personality_insights.profile(params, function(err, response) {
           node.status({});
           if (err) {
-            node.status({fill:'red', shape:'ring', text:'processing error'});
+            node.status({
+              fill: 'red',
+              shape: 'ring',
+              text: 'processing error',
+            });
             node.error(err, msg);
-          } else{
+          } else {
             msg.insights = response;
           }
 
           node.send(msg);
         });
-
       });
     });
   }
 
-  RED.nodes.registerType('watson-personality-insights-v3',Node,{
+  RED.nodes.registerType('watson-personality-insights-v3', Node, {
     credentials: {
-      username: {type:'text'},
-      password: {type:'password'}
-    }
+      username: { type: 'text' },
+      password: { type: 'password' },
+    },
   });
 };
